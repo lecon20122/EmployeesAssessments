fc66a233b222745774af0ed28aca699f7592f95f
lecon20122 <lecon20122@gmail.com>
2023-10-19 19:04:49 +0300

    updating the AssessmentAnswerVm

--- EmployeesAssessments.Application/Features/AssessmentAnswers/Query/GetAssessmentAnswers/AssessmentAnswerVm.cs
+++ EmployeesAssessments.Application/Features/AssessmentAnswers/Query/GetAssessmentAnswers/AssessmentAnswerVm.cs
@@ -1,15 +1,11 @@
-﻿using EmployeesAssessments.Domain.Entities;
-
-namespace EmployeesAssessments.Application.Features.AssessmentAnswers.Query.GetAssessmentAnswers
+﻿namespace EmployeesAssessments.Application.Features.AssessmentAnswers.Query.GetAssessmentAnswers
 {
     public class AssessmentAnswerVm
     {
         public long Id { get; set; }
-        public long AssessmentId { get; set; }
-        public Assessment Assessment { get; set; }
-        public long QuestionId { get; set; }
-        public AssessmentQuestion Question { get; set; }
         public string Answer { get; set; }
+        public long AssessmentId { get; set; }
+        public long QuestionId { get; set; }
         public long UserId { get; set; }
         public byte Score { get; set; }
     }
