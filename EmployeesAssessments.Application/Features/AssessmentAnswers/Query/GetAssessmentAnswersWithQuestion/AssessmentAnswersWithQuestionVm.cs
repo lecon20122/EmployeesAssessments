09b2517ecb4ec81acc9f9927b1f45da054b053b8
lecon20122 <lecon20122@gmail.com>
2023-10-22 10:57:23 +0300

    fixing QuestionId Property Name

--- EmployeesAssessments.Application/Features/AssessmentAnswers/Query/GetAssessmentAnswersWithQuestion/AssessmentAnswersWithQuestionVm.cs
+++ EmployeesAssessments.Application/Features/AssessmentAnswers/Query/GetAssessmentAnswersWithQuestion/AssessmentAnswersWithQuestionVm.cs
@@ -5,7 +5,7 @@
         public long Id { get; set; }
         public string Answer { get; set; }
         public long AssessmentId { get; set; }
-        public long QuestionId { get; set; }
+        public long AssessmentQuestionId { get; set; }
         public AssessmentQuestionDto AssessmentQuestion { get; set; }
         public long UserId { get; set; }
         public byte Score { get; set; }
